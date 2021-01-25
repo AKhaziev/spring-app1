2b8cd9dc306d8471d83aaa614cb467f4c2a964fe
AKhaziev <alexander.khaziev@yandex.com>
2021-01-25 19:42:13 +0500

    Lesson10

--- src/main/java/ru/akhaziev/MusicPlayer.java
+++ src/main/java/ru/akhaziev/MusicPlayer.java
@@ -1,14 +1,34 @@
 package ru.akhaziev;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-        private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+
+//    private ClassicalMusic classicalMusic;
+//    @Autowired
+//        private Music music;
 //    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+
+    //    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
     public String getName() {
         return name;
@@ -27,27 +47,33 @@ public class MusicPlayer {
     }
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    @Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic) {
+//        this.classicalMusic = classicalMusic;
+//    }
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 //    public MusicPlayer(List<Music> musicList) {
 //        this.musicList = musicList;
 //    }
 
-    public MusicPlayer() {
-    }
-
-        public void setMusic(Music music) {
-        this.music = music;
-    }
+//    public MusicPlayer() {
+//    }
+//
+//        public void setMusic(Music music) {
+//        this.music = music;
+//    }
 //    public void setMusicList(List<Music> musicList) {
 //        this.musicList = musicList;
 //    }
 
-    public void playMusic() {
+    public String playMusic() {
 //        for (Music music : musicList) {
 //            System.out.println(music);
-            System.out.println("Playing: " + music.getSong());
+        return "Playing: " + classicalMusic.getSong();
+//        System.out.println("Playing: " + rockMusic.getSong());
 //        }
     }
 }
